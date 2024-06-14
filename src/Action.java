40154d210a41daff4ba94c0e9dc1bbbbdb7a8284
Alex David Frías <adavidfruiz@gmail.com>
2024-06-14 02:28:38 -0500

    feat: add action class

---
+++ src/Action.java
@@ -0,0 +1,15 @@
+public class Action {
+    private String type;
+
+    public Action(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
